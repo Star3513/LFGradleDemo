94a6a2aeffae0338862d7714506c35a584c006d0
hanlz <hanlz@linkface.cn>
2018-03-29 09:47:45 +0800

    [android]add common utils

---
+++ LFGradleDemo/LFCommonUtils/src/main/java/com/linkface/demo/common/LFMyUtils.java
@@ -0,0 +1,11 @@
+package com.linkface.demo.common;
+
+/**
+ * Copyright (c) 2017-2018 LINKFACE Corporation. All rights reserved.
+ */
+
+public class LFMyUtils {
+    public static int calculatePlus(int a, int b) {
+        return a + b;
+    }
+}
---
+++ LFGradleDemo/app/src/main/java/com/linkface/demo/gradle/LFAppUtils.java
@@ -0,0 +1,11 @@
+package com.linkface.demo.gradle;
+
+/**
+ * Copyright (c) 2017-2018 LINKFACE Corporation. All rights reserved.
+ */
+
+public class LFAppUtils {
+    public static int calculatesubtract(int a, int b) {
+        return a - b;
+    }
+}
